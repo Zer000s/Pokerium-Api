475ff9663dadbb4c2fa78562c0ec7bf0f137232a
znach <znachok.egor@gmail.com>
2025-09-23 15:36:19 +0300

    init

---
+++ src/main/java/org/example/pokerium_api/PokeriumApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.pokerium_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeriumApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokeriumApiApplication.class, args);
+    }
+
+}
